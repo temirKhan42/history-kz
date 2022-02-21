f2e8efc65486c6bbe4538196d4b983a081b35d4e
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2022-02-22 00:15:50 +0600

    Add routes

---
+++ src/js/routes/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Home</h2>
+    </main>
+  );
+}
---
+++ src/js/routes/Progress.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Progress() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Progress</h2>
+    </main>
+  );
+}
