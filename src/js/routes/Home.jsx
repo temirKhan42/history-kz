b0b5b813bb42e3dbf8614cf92ae745d120ccb596
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2022-02-24 00:15:22 +0600

    Add form to Home page

--- src/js/routes/Home.jsx
+++ src/js/routes/Home.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { useFormik } from 'formik';
+
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: values => {
+      console.log(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default function Home() {
   return (
     <main style={{ padding: '1rem 0' }}>
-      <h2>Home</h2>
+      {SignupForm()}
     </main>
   );
 }
