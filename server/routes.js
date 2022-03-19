56ceb41fe09ed4c3feb25bb2974c5110c5f1ceda
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2022-03-19 19:37:17 +0600

    Mod server: add request to fetch chapter content

--- server/routes.js
+++ server/routes.js
@@ -1,37 +1,38 @@
 import _ from 'lodash';
 import HttpErrors from 'http-errors';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import * as fs from 'fs/promises';
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(__filename);
+
+const readfile = async (fileNum) => {
+  let filehandle;
+  try {
+    const filePath = path.join(__dirname, `../src/data/chapter${fileNum}.html`);
+    filehandle = await fs.open(filePath, 'r');
+    const file = await filehandle.readFile('utf-8');
+    return file;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await filehandle?.close();
+  }
+}
 
 const { Unauthorized, Conflict } = HttpErrors;
 
 const getNextId = () => Number(_.uniqueId());
 
-const buildState = (defaultState) => {
-  const generalChannelId = getNextId();
-  const randomChannelId = getNextId();
+const buildState = () => {
   const state = {
-    channels: [
-      { id: generalChannelId, name: 'general', removable: false },
-      { id: randomChannelId, name: 'random', removable: false },
-    ],
-    messages: [],
-    currentChannelId: generalChannelId,
     users: [
       { id: 1, email: 'admin@admin', password: 'admin', username: 'admin' },
     ],
   };
-
-  if (defaultState.messages) {
-    state.messages.push(...defaultState.messages);
-  }
-  if (defaultState.channels) {
-    state.channels.push(...defaultState.channels);
-  }
-  if (defaultState.currentChannelId) {
-    state.currentChannelId = defaultState.currentChannelId;
-  }
-  if (defaultState.users) {
-    state.users.push(...defaultState.users);
-  }
 
   return state;
 };
@@ -39,50 +40,50 @@ const buildState = (defaultState) => {
 export default (app, defaultState = {}) => {
   const state = buildState(defaultState);
 
-  app.io.on('connect', (socket) => {
-    console.log({ 'socket.id': socket.id });
+  // app.io.on('connect', (socket) => {
+  //   console.log({ 'socket.id': socket.id });
 
-    socket.on('newMessage', (message, acknowledge = _.noop) => {
-      const messageWithId = {
-        ...message,
-        id: getNextId(),
-      };
-      state.messages.push(messageWithId);
-      acknowledge({ status: 'ok' });
-      app.io.emit('newMessage', messageWithId);
-    });
+  //   socket.on('newMessage', (message, acknowledge = _.noop) => {
+  //     const messageWithId = {
+  //       ...message,
+  //       id: getNextId(),
+  //     };
+  //     state.messages.push(messageWithId);
+  //     acknowledge({ status: 'ok' });
+  //     app.io.emit('newMessage', messageWithId);
+  //   });
 
-    socket.on('newChannel', (channel, acknowledge = _.noop) => {
-      const channelWithId = {
-        ...channel,
-        removable: true,
-        id: getNextId(),
-      };
-      state.channels.push(channelWithId);
-      acknowledge({ status: 'ok', data: channelWithId });
-      app.io.emit('newChannel', channelWithId);
-    });
+  //   socket.on('newChannel', (channel, acknowledge = _.noop) => {
+  //     const channelWithId = {
+  //       ...channel,
+  //       removable: true,
+  //       id: getNextId(),
+  //     };
+  //     state.channels.push(channelWithId);
+  //     acknowledge({ status: 'ok', data: channelWithId });
+  //     app.io.emit('newChannel', channelWithId);
+  //   });
 
-    socket.on('removeChannel', ({ id }, acknowledge = _.noop) => {
-      const channelId = Number(id);
-      state.channels = state.channels.filter((c) => c.id !== channelId);
-      state.messages = state.messages.filter((m) => m.channelId !== channelId);
-      const data = { id: channelId };
+  //   socket.on('removeChannel', ({ id }, acknowledge = _.noop) => {
+  //     const channelId = Number(id);
+  //     state.channels = state.channels.filter((c) => c.id !== channelId);
+  //     state.messages = state.messages.filter((m) => m.channelId !== channelId);
+  //     const data = { id: channelId };
 
-      acknowledge({ status: 'ok' });
-      app.io.emit('removeChannel', data);
-    });
+  //     acknowledge({ status: 'ok' });
+  //     app.io.emit('removeChannel', data);
+  //   });
 
-    socket.on('renameChannel', ({ id, name }, acknowledge = _.noop) => {
-      const channelId = Number(id);
-      const channel = state.channels.find((c) => c.id === channelId);
-      if (!channel) return;
-      channel.name = name;
+  //   socket.on('renameChannel', ({ id, name }, acknowledge = _.noop) => {
+  //     const channelId = Number(id);
+  //     const channel = state.channels.find((c) => c.id === channelId);
+  //     if (!channel) return;
+  //     channel.name = name;
 
-      acknowledge({ status: 'ok' });
-      app.io.emit('renameChannel', channel);
-    });
-  });
+  //     acknowledge({ status: 'ok' });
+  //     app.io.emit('renameChannel', channel);
+  //   });
+  // });
 
   app.post('/api/v1/login', async (req, reply) => {
     const email = _.get(req, 'body.email');
@@ -119,6 +120,12 @@ export default (app, defaultState = {}) => {
       .send({ token, username, email });
   });
 
+  app.post('/api/v1/text', async (req, reply) => {
+    const chapterNum = _.get(req, 'body.chapterNum');
+    const file = await readfile(chapterNum);
+    reply.send({ file: file ? file : '' });
+  });
+
   app.get('/api/v1/data', { preValidation: [app.authenticate] }, (req, reply) => {
     const user = state.users.find(({ id }) => id === req.user.userId);
 
@@ -132,8 +139,7 @@ export default (app, defaultState = {}) => {
       .send(_.omit(state, 'users'));
   });
 
-  app
-    .get('*', (_req, reply) => {
-      reply.view('index.pug');
-    });
+  app.get('*', (_req, reply) => {
+    reply.view('index.pug');
+  });
 };
